d639a7f36681ae7c1bdfa70194c882cb598c1292
idesta <idesta705@gmail.com>
2026-01-22 12:45:29 +0300

    update profile route

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,12 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import {
+  login,
+  logout,
+  signup,
+  updateProfile,
+} from "../controllers/auth.controller.js";
+
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
@@ -9,6 +16,6 @@ router.post("/login", login);
 
 router.post("/logout", logout);
 
-router.put("/update-profile", updateProfile);
+router.put("/update-profile", protectRoute, updateProfile);
 
 export default router;
