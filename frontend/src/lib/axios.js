1a3af46b066ca65aefb151c29a0839a4f904125b
idesta <idesta705@gmail.com>
2026-04-06 16:03:00 +0300

    convert the hard-coded front-end-url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5001";
+
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: `${BACKEND_URL}/api`,
   withCredentials: true,
 });
