f47ab0547cf3d36a7a72c578fc4df6de9443fb53
idesta <idesta705@gmail.com>
2026-04-06 16:02:24 +0300

    convert the hard-coded front-end-url

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,12 +12,13 @@ dotenv.config();
 // const app = express();  Used in WebSocket
 
 const PORT = process.env.PORT;
+const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173";
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: CLIENT_URL,
     credentials: true,
   }),
 );
